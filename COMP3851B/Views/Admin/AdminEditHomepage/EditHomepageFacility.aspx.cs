a0a1589ae0e300193b6d86ef4604b22738c395c9
gtan8510 <106289192+gtan8510@users.noreply.github.com>
2022-11-09 22:59:33 +0800

    Update EditHomepageFacility.aspx.cs

--- COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageFacility.aspx.cs
+++ COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageFacility.aspx.cs
@@ -19,7 +19,7 @@ namespace COMP3851B.Views.Admin.AdminEditHomepage
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
-            if (txtFacilityName.Text == "")
+             if (txtFacilityName.Text == "")
             {
                 lblNotice.Text = " Please enter the quote";
             }
@@ -33,30 +33,44 @@ namespace COMP3851B.Views.Admin.AdminEditHomepage
             }
             else
             {
-                string fileName = Path.GetFileName(FuFacilityImg.PostedFile.FileName);
-                string contenType = FuFacilityImg.PostedFile.ContentType;
-                using (Stream fs = FuFacilityImg.PostedFile.InputStream)
-                {
-                    using (BinaryReader br = new BinaryReader(fs))
-                    {
-                        byte[] bytes = br.ReadBytes((Int32)fs.Length);
-                        string constr = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
-                        using (SqlConnection con = new SqlConnection(constr))
-                        {
-                            string query = "Insert into campusFacility values ('" + txtFacilityName.Text + "', '" + txtFacilityDesc.Text + "', '" + FuFacilityImg.FileName + "')";
-                            using (SqlCommand cmd = new SqlCommand(query))
-                            {
-                                cmd.Connection = con;
-                                con.Open();
-                                cmd.ExecuteNonQuery();
-                                con.Close();
-                                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully inserted');", true);
-                            }
-                        }
-                    }
-                }
+                string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+                SqlConnection con = new SqlConnection(DbConnect);
+                FuFacilityImg.SaveAs(Request.PhysicalApplicationPath + "./Images/" + FuFacilityImg.FileName.ToString());
+                a = "Images/" + a + FuFacilityImg.FileName.ToString();
 
-            }
+                string ins = "insert into campusFacility (facilityName, facilityDesc, facilityPict) values('" + txtFacilityName.Text + "', '" + txtFacilityDesc.Text +"', '" + a.ToString() + "')";
+                SqlCommand com = new SqlCommand(ins, con);
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+   
+           
         }
     }
+    
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(DbConnect);
+            string ins = "Select * from campusFacility where (facilityName like '%' +@facilityName + '%' or facilityDesc like '%' +@facilityDesc + '%')";
+            SqlCommand cmd = new SqlCommand(ins, con);
+            cmd.Parameters.Add("@facilityName", SqlDbType.NVarChar).Value = txtSearch.Text;
+            cmd.Parameters.Add("@facilityDesc", SqlDbType.NVarChar).Value = txtSearch.Text;
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "facilityName");
+            da.Fill(ds, "facilityDesc");
+
+            GridView1.DataSourceID = null;
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+            lblNotice.Text = "Data has been selected";
+        }
+}
 }

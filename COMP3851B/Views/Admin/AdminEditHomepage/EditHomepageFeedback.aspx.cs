9d51866c83d7e68415b156991d9672374ab06428
gtan8510 <106289192+gtan8510@users.noreply.github.com>
2022-11-09 23:01:52 +0800

    Update EditHomepageFeedback.aspx.cs

--- COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageFeedback.aspx.cs
+++ COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageFeedback.aspx.cs
@@ -15,85 +15,33 @@ namespace COMP3851B.Views.Admin.AdminEditHomepage
         public List<Feedback> feedback;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["uname"].ToString() != "admin")
-            {
-                ScriptManager.RegisterStartupScript(this, this.GetType(),
-                  "alert",
-               "alert('This page is only accessible to Admins');window.location ='UserHome.aspx';",
-               true);
-
-            }
-            else
-            {
-                if (IsPostBack == false)
-                {
-                    txtFeedbackPersonName.Text = "";
-                    txtFeedbackDesc.Text = "";
-
-
-
-                }
-            }
+            
+            
         }
-
-        protected void btnCreate_Click(object sender, EventArgs e)
+        
+         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            try
-            {
-                //Get text fields and add new course
-                string textFeedbackPersonName = txtFeedbackPersonName.Text;
-                string textFeedbackDesc = txtFeedbackDesc.Text;
+            string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(DbConnect);
+            string ins = "Select * from studentFeedback where (studentName like '%' + @studentName + '%' or studentFeedback like '%' +@studentFeedback + '%')";
+            SqlCommand cmd = new SqlCommand(ins, con);
+            cmd.Parameters.Add("@studentName", SqlDbType.NVarChar).Value = txtSearch.Text;
+            cmd.Parameters.Add("@studentFeedback", SqlDbType.NVarChar).Value = txtSearch.Text;
+            con.Open();
+            cmd.ExecuteNonQuery();
 
-                /*
-                //Create folder and file paths
-                var folder = Server.MapPath("~/uploadsCourse/");
-                string fileName = Path.GetFileName(FuImgCourse.PostedFile.FileName);
-                string filePath = "~/uploads/" + fileName;
-
-               lblCourseImage.Text = fileName.ToString();
-
-                //create new directory for product images
-                if (!Directory.Exists(folder))
-                {
-                    Directory.CreateDirectory(folder);
-                    //click 'Show All Files' to find folder
-
-                }
-                FuImgCourse.PostedFile.SaveAs(Server.MapPath(filePath));
-                */
-                //Initialise new Product object and add to db
-                Feedback feedback = new Feedback(textFeedbackPersonName, textFeedbackDesc);
-                int result = feedback.AddFeedback();
-
-
-                if (result == 1) //add successful
-                {
-                    ScriptManager.RegisterStartupScript(this, this.GetType(),
-                        "alert",
-                        "alert('New product added successfully!');window.location ='ListItem.aspx';",
-                        true);
-                }
-                else
-                {
-                    ScriptManager.RegisterStartupScript(this, this.GetType(),
-                        "alert",
-                        "alert('An error occured while adding product, please try again');window.location ='ListItem.aspx';",
-                        true);
-                }
-            }
-            catch
-            {
-                ScriptManager.RegisterStartupScript(this, this.GetType(),
-                    "alert",
-                    "alert('An error has occured. Please contact the developers to fix this issue.');window.location ='AddItem.aspx';",
-                    true);
-            }
-
-
-
-
-
-
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "studentName");
+            da.Fill(ds, "studentFeedback");
+            GridViewStudentFeedback.DataSourceID = null;
+            GridViewStudentFeedback.DataSource = ds;
+            GridViewStudentFeedback.DataBind();
+            con.Close();
+            lblNotice.Text = "Data has been selected";
         }
+
+       
     }
     }

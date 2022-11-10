4b07095658f8a88d2583809be417945c42d7dc99
darma121200 <105428112+darma121200@users.noreply.github.com>
2022-11-10 12:32:23 +0800

    Add files via upload

---
+++ COMP3851B/Views/Admin/Campus/CampusAchievement.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP3851B.Views.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string a;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-ANLBS8M\\SQLEXPRESS;Initial Catalog=COMP3851B;Integrated Security=True");
+
+        protected void btnAdd_Click1(object sender, EventArgs e)
+        {
+            string fileName = Path.GetFileName(fuAchievementImage.PostedFile.FileName);
+            string contentType = fuAchievementImage.PostedFile.ContentType;
+            using (Stream fs = fuAchievementImage.PostedFile.InputStream)
+            {
+                string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+                SqlConnection con = new SqlConnection(DbConnect);
+                fuAchievementImage.SaveAs(Request.PhysicalApplicationPath + "./Images/" + fuAchievementImage.FileName.ToString());
+                a = "Images/" + a + fuAchievementImage.FileName.ToString();
+
+                string ins = "insert into campusAchievement (achievementName, achievementImage) values('" + txtAchievementName.Text + "', '" + a.ToString() + "')";
+                SqlCommand com = new SqlCommand(ins, con);
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+            }
+        }
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("select * from campusAchievement", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            gridView.DataSource = dt;
+            gridView.DataBind();
+        }
+
+        
+    }   
+}
---
+++ COMP3851B/Views/Admin/Campus/CampusEvent.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP3851B.Views.Admin
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        string a;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-ANLBS8M\\SQLEXPRESS;Initial Catalog=COMP3851B;Integrated Security=True");
+
+
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("select * from campusEvent", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            gridView.DataSource = dt;
+            gridView.DataBind();
+        }
+
+        protected void btnAdd_Click1(object sender, EventArgs e)
+        {
+            string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(DbConnect);
+            fuEventImage.SaveAs(Request.PhysicalApplicationPath + "./Images/" + fuEventImage.FileName.ToString());
+            a = "Images/" + a + fuEventImage.FileName.ToString();
+
+            string ins = "insert into campusEvent (eventName, eventImage) values('" + txtEventName.Text + "', '" + a.ToString() + "')";
+            SqlCommand com = new SqlCommand(ins, con);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+        }
+
+        
+    }
+}

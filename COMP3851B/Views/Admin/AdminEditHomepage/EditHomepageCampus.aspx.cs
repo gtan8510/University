ebd772f48e181162ead7378ee685fbbc7e76466c
gtan8510 <106289192+gtan8510@users.noreply.github.com>
2022-11-12 21:45:14 +0800

    Update EditHomepageCampus.aspx.cs

--- COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageCampus.aspx.cs
+++ COMP3851B/Views/Admin/AdminEditHomepage/EditHomepageCampus.aspx.cs
@@ -11,6 +11,8 @@ namespace COMP3851B.Views.Admin.AdminEditHomepage
 {
     public partial class EditHomepageCampus : System.Web.UI.Page
     {
+    
+         string a;
         
         protected void Page_Load(object sender, EventArgs e)
         {

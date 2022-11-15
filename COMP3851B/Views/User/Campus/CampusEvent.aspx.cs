6e5c093cf5da979cb696da1d7bd6a07a1c6e4a63
darma121200 <105428112+darma121200@users.noreply.github.com>
2022-11-15 12:31:56 +0800

    Add files via upload

---
+++ COMP3851B/Views/User/Campus/CampusEvent.aspx.cs
@@ -0,0 +1,39 @@
+﻿using COMP3851B.BBL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP3851B.Views.User.Campus
+{
+    public partial class campusEvent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            loadEvent();
+        }
+        void loadEvent()
+        {
+            string DbConnect = ConfigurationManager.ConnectionStrings["FunUniversityConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(DbConnect);
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select top(15) [eventName], [eventImage] from [campusEvent] Order By eventID desc";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            LVEvent.DataSource = dt;
+            LVEvent.DataBind();
+            con.Close();
+        }
+    }
+}
